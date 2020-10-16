11944a682831088af6619aef341e0f1b18d8e92d
shgtkshruch <shgtk.shruch@gmail.com>
2020-10-16 19:43:44 +0900

    Assign flow to workflow instance

--- dag.rb
+++ dag.rb
@@ -3,27 +3,43 @@ require 'tsort'
 class Workflow
   include TSort
 
+  attr_accessor :flow
+
+  def initialize(flow:)
+    @flow = flow
+  end
+
   def tsort_each_child(node)
-    g[node].each do |child|
+    flow[node].each do |child|
       yield child
     end
   end
 
   def tsort_each_node
-    g.each_key do |node|
+    flow.each_key do |node|
       yield node
     end
   end
 
-  def g
-    { a: [:b, :c], b: [:d], c: [:b, :d], d: [] }
-  end
-
-  def go
+  def static_order
     each_strongly_connected_component do |node|
       p node
     end
   end
 end
 
-Workflow.new.go
+flow = {
+  ca: [],
+  kr: [:ca],
+  ba: [:kr],
+  ua: [:ca],
+  qt: [:ua],
+  da: [
+    :ba,
+    :ua
+  ],
+  rc: [:da],
+  rp: [:rc]
+}
+
+Workflow.new(flow: flow).static_order
