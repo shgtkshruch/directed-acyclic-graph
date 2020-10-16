b833bb6d69f735344d3e1b5d0438c066e33d33ba
shgtkshruch <shgtk.shruch@gmail.com>
2020-10-17 08:54:12 +0900

    Refactor with `&method`

--- workflow/workflow.rb
+++ workflow/workflow.rb
@@ -16,20 +16,20 @@ class Workflow
   end
 
   def get_ready
-    not_done.filter { |node| get_ready?(node) }
+    not_done.filter(&method(:get_ready?))
   end
 
   def get_ready?(node)
     each_strongly_connected_component_from(node) do |nodes|
       next if nodes.include? node
       # TODO: return reason why this node is not ready
-      return false if nodes.any? { |n| !done?(n) }
+      return false if !nodes.all?(&method(:done?))
     end
     true
   end
 
   def done
-    static_order.filter { |node| done?(node) }
+    static_order.filter(&method(:done?))
   end
 
   def not_done
@@ -39,7 +39,7 @@ class Workflow
   def valid?
     static_order.all? do |node|
       each_strongly_connected_component_from(node) do |nodes|
-        return done?(node) && nodes.all? { |n| done?(n) }
+        return done?(node) && nodes.all?(&method(:done?))
       end
     end
   end
